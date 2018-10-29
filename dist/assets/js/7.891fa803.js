cb8208f804038fbed3b49bee149f21983365fd14
sagarmahapatra <sagarmahapatra@gmail.com>
2018-10-29 15:26:42 +0530

    asd

---
+++ dist/assets/js/7.891fa803.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{162:function(t,e,n){"use strict";n.r(e);var r=n(0),s=Object(r.a)({},function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"content"},[e("Index",{attrs:{title:"Curry",path:"/recipes/curry"}})],1)},[],!1,null,null,null);e.default=s.exports}}]);
---
+++ dist/assets/js/9.8fd1e3bf.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{158:function(t,e,n){"use strict";n.r(e);var s=n(0),c=Object(s.a)({},function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"content"},[e("Index",{attrs:{title:"Recipes",path:"/recipes"}})],1)},[],!1,null,null,null);e.default=c.exports}}]);
